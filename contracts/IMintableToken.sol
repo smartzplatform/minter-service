3d85dff47b38775d9e681c2183717c4b6a006280
Eenae <z-line@my.com>
2017-12-01 17:36:08 +0300

    updating compiler

--- contracts/IMintableToken.sol
+++ contracts/IMintableToken.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.15;
+pragma solidity ^0.4.15;
 
 contract IMintableToken {
     function mint(address _to, uint256 _amount);
--- test/solidity/TestReenterableMinter.sol
+++ test/solidity/TestReenterableMinter.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.15;
+pragma solidity ^0.4.15;
 
 import '../../contracts/ReenterableMinter.sol';
 import '../../contracts/test_helpers/SimpleMintableToken.sol';
